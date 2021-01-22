d796da95858b0f7ce2e95a68765596d3804d96c7
Rich <richelton14@gmail.com>
2021-01-22 16:23:36 -0300

    adicionado get by id

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -8,5 +8,6 @@ export default function routes(app: Express) {
     app.post('/companies', validateBodyDTO(CompanyCreate), (req, res) => CompanyController.store(req, res));
     app.put('/companies/:id', validateParamsDTO(CompanyUpdateAndDelete), validateBodyDTO(CompanyUpdate), (req, res) => CompanyController.update(req, res));
     app.get('/companies', (req, res) => CompanyController.index(req, res));
+    app.get('/companies/:id', validateParamsDTO(CompanyUpdateAndDelete), (req, res) => CompanyController.get(req, res));
     app.delete('/companies/:id', validateParamsDTO(CompanyUpdateAndDelete), (req, res) => CompanyController.delete(req, res));
 }
