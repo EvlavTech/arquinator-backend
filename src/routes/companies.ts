7027aa87d8b07b9829190bacab46964dca258692
Rich <richelton14@gmail.com>
2021-01-22 16:03:00 -0300

    Adicionado DTO para validação dos campos de Company

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express';
+import { validateBodyDTO, validateParamsDTO } from '@middlewares/validateDTO';
+import { CompanyCreate, CompanyUpdate, CompanyUpdateAndDelete } from '@dto/Company/CompanyDTO';
 
 import CompanyController from '@controllers/CompanyController';
 
 export default function routes(app: Express) {
-    app.post('/companies', (req, res) => CompanyController.store(req, res));
-    app.put('/companies/:id', (req, res) => CompanyController.update(req, res));
+    app.post('/companies', validateBodyDTO(CompanyCreate), (req, res) => CompanyController.store(req, res));
+    app.put('/companies/:id', validateParamsDTO(CompanyUpdateAndDelete), validateBodyDTO(CompanyUpdate), (req, res) => CompanyController.update(req, res));
     app.get('/companies', (req, res) => CompanyController.index(req, res));
-    app.delete('/companies/:id', (req, res) => CompanyController.delete(req, res));
+    app.delete('/companies/:id', validateParamsDTO(CompanyUpdateAndDelete), (req, res) => CompanyController.delete(req, res));
 }
