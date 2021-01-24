cca1d4ec4e4d72f7b877f80cbcc4f7d0a0e3f49c
Rich <richelton14@gmail.com>
2021-01-24 15:12:08 -0300

    Alteração no modelo de UserPermission

--- src/app/models/UserPermission.ts
+++ src/app/models/UserPermission.ts
@@ -1,23 +1,46 @@
-import { Model } from 'sequelize';
+import DataTypes, { Sequelize } from 'sequelize';
+import GenericModel from './GenericModel';
 
-import database from '@database/index';
 
 export interface IUserPermission {
     userId: number;
     permissionId: number;
 }
 
-class UserPermission extends Model {
+class UserPermission extends GenericModel {
     public userId!: number;
     public permissionId!: number;
-}
 
-UserPermission.init(
-    {},
-    {
-        sequelize: database.connection,
-        tableName: 'UserPermissions',
-    },
-);
+    public static initModel(connection: Sequelize): void {
+        UserPermission.init(
+            {
+                userId: {
+                    allowNull: false,
+                    references: {
+                        model: 'Users',
+                        key: 'id',
+                    },
+                    type: DataTypes.INTEGER,
+                    onDelete: 'CASCADE',
+                    onUpdate: 'CASCADE',
+                },
+                permissionId: {
+                    allowNull: false,
+                    references: {
+                        model: 'Permissions',
+                        key: 'id',
+                    },
+                    type: DataTypes.INTEGER,
+                    onDelete: 'CASCADE',
+                    onUpdate: 'CASCADE',
+                }
+            },
+            {
+                sequelize: connection,
+                tableName: 'UserPermissions',
+            },
+        );
+    }
+}
 
 export default UserPermission;
