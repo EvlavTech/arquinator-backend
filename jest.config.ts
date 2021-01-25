a4e717c16ad2c78134cb854b9b3eb2ee7a97ca0b
Rich <richelton14@gmail.com>
2021-01-25 19:29:38 -0300

    Iniciando com testes

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '@core/(.*)': '<rootDir>/src',
+        '@controllers/(.*)': '<rootDir>/src/app/controllers/$1',
+        '@models/(.*)': '<rootDir>/src/app/models/$1',
+        '@dto/(.*)': '<rootDir>/src/app/dto/$1',
+        '@repositories/(.*)': '<rootDir>/src/app/repository/$1',
+        '@middlewares/(.*)': '<rootDir>/src/app/middlewares/$1',
+        '@config/(.*)': '<rootDir>/src/config/$1',
+        '@database/(.*)': '<rootDir>/src/database/$1',
+    },
+};
