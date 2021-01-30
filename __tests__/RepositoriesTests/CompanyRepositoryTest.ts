1ea1a7ff54bb9063c11a494145ea2a98bcf08a63
Rich <richelton14@gmail.com>
2021-01-30 18:12:42 -0300

    Alterações nos testes de Company e Client e adicionado os de Project

--- __tests__/RepositoriesTests/CompanyRepositoryTest.ts
+++ __tests__/RepositoriesTests/CompanyRepositoryTest.ts
@@ -2,14 +2,18 @@
 /* eslint-disable consistent-return */
 import '../../src/app';
 import CompanyRepository from '../../src/app/repository/CompanyRepository';
+import { ICompany } from '../../src/app/models/Company';
+
+let company_created_1: ICompany;
+let company_created_2: ICompany;
 
 describe('Tests company repository', () => {
     it('Test create company', async () => {
-        const company_created_1 = await CompanyRepository.create({
+        company_created_1 = await CompanyRepository.create({
             name: 'Company Test 1',
         });
 
-        const company_created_2 = await CompanyRepository.create({
+        company_created_2 = await CompanyRepository.create({
             name: 'Company Test 2',
         });
 
@@ -51,61 +55,61 @@ describe('Tests company repository', () => {
     });
 
     it('Test get companies with filters with id is equal 1', async () => {
-        const companies = await CompanyRepository.findByFilters({ id: 1 });
+        const companies = await CompanyRepository.findByFilters({ id: company_created_1.id });
 
         expect(companies).toMatchObject([
-            { name: 'Company Test 1', id: 1 },
+            { name: 'Company Test 1', id: company_created_1.id },
         ]);
         expect(companies.length).toEqual(1);
     });
 
     it('Test get companies with filters with id is not exists', async () => {
-        const companies = await CompanyRepository.findByFilters({ id: 5 });
+        const companies = await CompanyRepository.findByFilters({ id: 150 });
 
         expect(companies).toMatchObject([]);
     });
 
     it('Test get by id companies', async () => {
-        const companies = await CompanyRepository.findById(2);
+        const companies = await CompanyRepository.findById(company_created_2.id);
 
-        expect(companies).toMatchObject({ name: 'Company Test 2', id: 2 });
+        expect(companies).toMatchObject({ name: 'Company Test 2', id: company_created_2.id });
     });
 
     it('Test get by id companies with id not exists', async () => {
-        const companies = await CompanyRepository.findById(7);
+        const companies = await CompanyRepository.findById(150);
 
         expect(companies).toEqual(null);
     });
 
     it('Test update companies', async () => {
-        const companies = await CompanyRepository.update(2, { name: 'Company Test 2 Update' });
-        const company = await CompanyRepository.findById(2);
+        const companies = await CompanyRepository.update(company_created_2.id, { name: 'Company Test 2 Update' });
+        const company = await CompanyRepository.findById(company_created_2.id);
 
         expect(companies[0]).toEqual(1);
-        expect(company).toMatchObject({ name: 'Company Test 2 Update', id: 2 });
+        expect(company).toMatchObject({ name: 'Company Test 2 Update', id: company_created_2.id });
     });
 
     it('Test update companies with id not exists', async () => {
-        const companies = await CompanyRepository.update(5, { name: 'Company 3 Test Update' });
+        const companies = await CompanyRepository.update(150, { name: 'Company 3 Test Update' });
 
         expect(companies[0]).toEqual(0);
     });
 
     it('Test delete companies', async () => {
-        const company = await CompanyRepository.delete(2);
-        const company_deleted = await CompanyRepository.findById(2);
+        const company = await CompanyRepository.delete(company_created_2.id);
+        const company_deleted = await CompanyRepository.findById(company_created_2.id);
 
-        expect(company).toMatchObject({ name: 'Company Test 2 Update', id: 2 });
+        expect(company).toMatchObject({ name: 'Company Test 2 Update', id: company_created_2.id });
         expect(company_deleted).toEqual(null);
     });
 
     it('Test delete companies with id not exists', async () => {
-        const company = await CompanyRepository.delete(5);
+        const company = await CompanyRepository.delete(150);
 
         expect(company).toEqual(null);
     });
 
     afterAll(async () => {
-        await CompanyRepository.delete(3);
+        await CompanyRepository.delete(company_created_1.id);
     });
 });
---
+++ __tests__/RepositoriesTests/ProjectsRepositoryTest.ts
@@ -0,0 +1,175 @@
+/* eslint-disable no-undef */
+/* eslint-disable consistent-return */
+import '../../src/app';
+import CompanyRepository from '../../src/app/repository/CompanyRepository';
+import ClientRepository from '../../src/app/repository/ClientRepository';
+import ProjectRepository from '../../src/app/repository/ProjectRepository';
+import { ICompany } from '../../src/app/models/Company';
+
+let company_created_1: ICompany;
+
+describe('Tests project repository', () => {
+    it('Test create project', async () => {
+        company_created_1 = await CompanyRepository.create({
+            name: 'Company Test 1',
+        });
+
+        const client_created_1 = await ClientRepository.create({
+            name: 'Client Test 1',
+            company_id: company_created_1.id,
+        });
+
+        const project_created_1 = await ProjectRepository.create({
+            name: 'Project Test 1',
+            description: 'Description project 1',
+            owner_id: client_created_1.id,
+            start_date: '2020-12-17T03:00:00.000Z',
+            end_date: '2020-12-25T03:00:00.000Z',
+        });
+
+        const project_created_2 = await ProjectRepository.create({
+            name: 'Project Test 2',
+            description: 'Description project 2',
+            owner_id: client_created_1.id,
+            start_date: '2020-12-20T03:00:00.000Z',
+            end_date: '2020-12-30T03:00:00.000Z',
+        });
+
+        expect(project_created_1.name).toBe('Project Test 1');
+        expect(project_created_1.description).toBe('Description project 1');
+        expect(project_created_1.start_date).toStrictEqual(new Date('2020-12-17T03:00:00.000Z'));
+        expect(project_created_1.end_date).toStrictEqual(new Date('2020-12-25T03:00:00.000Z'));
+
+        expect(project_created_2.name).toBe('Project Test 2');
+        expect(project_created_2.description).toBe('Description project 2');
+        expect(project_created_2.start_date).toStrictEqual(new Date('2020-12-20T03:00:00.000Z'));
+        expect(project_created_2.end_date).toStrictEqual(new Date('2020-12-30T03:00:00.000Z'));
+    });
+
+    it('Test get all projects', async () => {
+        const projects = await ProjectRepository.findAll();
+
+        expect(projects).toMatchObject([
+            {
+                name: 'Project Test 1',
+                description: 'Description project 1',
+                start_date: new Date('2020-12-17T03:00:00.000Z'),
+                end_date: new Date('2020-12-25T03:00:00.000Z'),
+            },
+            {
+                name: 'Project Test 2',
+                description: 'Description project 2',
+                start_date: new Date('2020-12-20T03:00:00.000Z'),
+                end_date: new Date('2020-12-30T03:00:00.000Z'),
+            },
+        ]);
+    });
+
+    it('Test get all projects with error', async () => {
+        const projects = await ProjectRepository.findAll();
+
+        expect(projects).not.toMatchObject([
+            { name: 'Company Test 1' },
+        ]);
+    });
+
+    it('Test get projects with filters', async () => {
+        const projects = await ProjectRepository.findByFilters({ name: 'Project Test 1' });
+
+        expect(projects).toMatchObject([
+            {
+                name: 'Project Test 1',
+                description: 'Description project 1',
+                start_date: new Date('2020-12-17T03:00:00.000Z'),
+                end_date: new Date('2020-12-25T03:00:00.000Z'),
+            },
+        ]);
+
+        expect(projects.length).toEqual(1);
+    });
+
+    it('Test get projects with filters with name is not exists', async () => {
+        const projects = await ProjectRepository.findByFilters({ name: 'Project Test' });
+
+        expect(projects).toMatchObject([]);
+    });
+
+    it('Test get projects with filters with id is equal 1', async () => {
+        const projects = await ProjectRepository.findByFilters({ id: 1 });
+
+        expect(projects).toMatchObject([
+            {
+                name: 'Project Test 1',
+                description: 'Description project 1',
+                start_date: new Date('2020-12-17T03:00:00.000Z'),
+                end_date: new Date('2020-12-25T03:00:00.000Z'),
+            },
+        ]);
+        expect(projects.length).toEqual(1);
+    });
+
+    it('Test get projects with filters with id is not exists', async () => {
+        const projects = await ProjectRepository.findByFilters({ id: 5 });
+
+        expect(projects).toMatchObject([]);
+    });
+
+    it('Test get by id projects', async () => {
+        const projects = await ProjectRepository.findById(2);
+
+        expect(projects).toMatchObject({
+            name: 'Project Test 2',
+            description: 'Description project 2',
+            start_date: new Date('2020-12-20T03:00:00.000Z'),
+            end_date: new Date('2020-12-30T03:00:00.000Z'),
+        });
+    });
+
+    it('Test get by id projects with id not exists', async () => {
+        const projects = await ProjectRepository.findById(7);
+
+        expect(projects).toEqual(null);
+    });
+
+    it('Test update projects', async () => {
+        const projects = await ProjectRepository.update(2, { name: 'Project Test 2 Update' });
+        const project = await ProjectRepository.findById(2);
+
+        expect(projects[0]).toEqual(1);
+        expect(project).toMatchObject({
+            name: 'Project Test 2 Update',
+            description: 'Description project 2',
+            start_date: new Date('2020-12-20T03:00:00.000Z'),
+            end_date: new Date('2020-12-30T03:00:00.000Z'),
+        });
+    });
+
+    it('Test update projects with id not exists', async () => {
+        const projects = await ProjectRepository.update(5, { name: 'Company 3 Test Update' });
+
+        expect(projects[0]).toEqual(0);
+    });
+
+    it('Test delete projects', async () => {
+        const project = await ProjectRepository.delete(2);
+        const project_deleted = await ProjectRepository.findById(2);
+
+        expect(project).toMatchObject({
+            name: 'Project Test 2 Update',
+            description: 'Description project 2',
+            start_date: new Date('2020-12-20T03:00:00.000Z'),
+            end_date: new Date('2020-12-30T03:00:00.000Z'),
+        });
+        expect(project_deleted).toEqual(null);
+    });
+
+    it('Test delete companies with id not exists', async () => {
+        const project = await ProjectRepository.delete(5);
+
+        expect(project).toEqual(null);
+    });
+
+    afterAll(async () => {
+        await CompanyRepository.delete(company_created_1.id);
+    });
+});
